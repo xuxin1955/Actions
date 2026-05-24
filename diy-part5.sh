9021853eb0452972549cb219c34bc787294ed995
xuxin1955 <x2874836915@qq.com>
2026-05-24 22:41:20 +0800

    update

--- diy-part5.sh
+++ diy-part5.sh
@@ -6,4 +6,8 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 
 # hlk7620A dts
 mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7620a_zbtlink_zbt-we826.dtsi" "target/linux/ramips/dts/mt7620a_zbtlink_zbt-we826.dtsi"
+cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_zbtlink_zbt-we826.dtsi" "target/linux/ramips/dts/mt7620a_zbtlink_zbt-we826.dtsi"
+
+
+mkdir -p "target/linux/ramips/mt7620/base-files/etc/board.d/"
+cp -f "$GITHUB_WORKSPACE/scripts/mt7620/01_leds" "target/linux/ramips/mt7620/base-files/etc/board.d/01_leds"
