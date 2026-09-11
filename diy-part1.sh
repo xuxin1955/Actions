dc061d5e900541da030e7118881c8251155bf154
xuxin1955 <x2874836915@qq.com>
2026-09-11 15:50:09 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,27 +7,18 @@ cp -f "$GITHUB_WORKSPACE/scripts/dts/msm8916.dtsi" "target/linux/msm89xx/dts/msm
 # OpenClash
 # git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
+# OpenAppFilter
+git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # 调试
 sed -i 's|src-git-full openstick https://github.com/lkiuyu/openstick-feeds.git|src-git-full openstick https://github.com/xuxin1955/openstick-feeds|g' feeds.conf.default
 
-# test
-# cat > feeds.conf.default << 'EOF'
-# src-git packages https://github.com/immortalwrt/packages.git;openwrt-25.12
-# src-git luci https://github.com/immortalwrt/luci.git;openwrt-25.12
-# src-git routing https://github.com/openwrt/routing.git;openwrt-25.12
-# src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12
-# src-git video https://github.com/openwrt/video.git;openwrt-25.12
-# src-git-full openstick https://github.com/xuxin1955/openstick-feeds.git
-# EOF
 
 # inspect
 echo ">>> OPP VOLTAGE COUNT: $(grep -c 'opp-microvolt' target/linux/msm89xx/dts/msm8916.dtsi) <<<"
 
 
-# istore
-echo >> feeds.conf.default
-echo 'src-git istore https://github.com/xuxin1955/istore;main' >> feeds.conf.default
